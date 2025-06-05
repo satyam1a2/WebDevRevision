4d6d4c46c0a6d10e13a8a4517b9f18244040cc0f
satyam1a2 <satyamkumar1a2@gmail.com>
2025-06-05 13:24:38 +0530

    class bassed component done

--- React/classBassedComponents/src/Counter.jsx
+++ React/classBassedComponents/src/Counter.jsx
@@ -7,12 +7,32 @@ class Counter extends React.Component {
         this.state = {
             count: 0
         };
+        console.log("Constructor called");
+    }
+
+    componentDidMount(){  // useEffect, [] (empty array)
+        console.log("Component did mount called");
+        // API call can be made here
+        this.setState({ count: this.state.count+1 }); // Example of setting initial state after mount
+    }
+
+    componentDidUpdate(){  // useEffet, __(No array)
+        console.log("Component did update called");
+        // This method is called after every update
+        // You can perform actions based on state changes here
+    }
+
+    componentWillUnmount(){  // Cleanup function in useEffect
+        console.log("Component will unmount called");
+        // This method is called before the component is removed from the DOM
+        // Cleanup actions can be performed here
     }
 
  
     
     render(){
 
+        console.log("Render called");
         return (
             <>
             <div>
